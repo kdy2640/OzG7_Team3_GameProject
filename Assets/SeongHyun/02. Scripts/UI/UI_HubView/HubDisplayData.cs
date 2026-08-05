c43d7b14ada4a105d2216bc98289e955076828d3
김성현A <tunapig@hanmail.net>
2026-08-05 18:33:05 +0900

    KSH 08.05

--- Assets/SeongHyun/02. Scripts/UI/UI_HubView/HubDisplayData.cs
+++ Assets/SeongHyun/02. Scripts/UI/UI_HubView/HubDisplayData.cs
@@ -3,22 +3,13 @@ using System;
 [Serializable]
 public class HubDisplayData
 {
-    // 현재 보유 재화
     public int Gold;
 
-    // 1 ~ 4
     public int PlayerLevel;
 
     public string PlayerName;
 
-    // 현재 누적 매출액
-    public int CurrentSales;
-
-    // 현재 승급 목표 매출액
-    public int TargetSales;
-
-    // 0 ~ 5
-    public int PromotionStep;
+    public int TotalGold;
 
     public string PromotionQuestTitle;
 
