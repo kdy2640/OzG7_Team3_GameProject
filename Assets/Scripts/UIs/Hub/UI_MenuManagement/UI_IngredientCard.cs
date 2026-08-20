62c2189d5bf26eac71fae361c0e228465027083b
Doyeop Kim <rlaehduq2640@gmail.com>
2026-08-20 14:50:56 +0900

    Polish GroceryView

--- Assets/Scripts/UIs/Hub/UI_MenuManagement/UI_IngredientCard.cs
+++ Assets/Scripts/UIs/Hub/UI_MenuManagement/UI_IngredientCard.cs
@@ -8,16 +8,19 @@ public sealed class UI_IngredientCard : MonoBehaviour
 
     [SerializeField] private Image icon;
     [SerializeField] private TMP_Text countText;
+    [SerializeField] private GroceryType nowType;
 
     public void SetData(GroceryAmount ingredient, int ownedAmount)
     {
         if (ingredient == null)
             return;
-
+        nowType = ingredient.grocery;
         countText.text = $"{ownedAmount} / {ingredient.amount}";
 
         countText.color = 
             ownedAmount >= ingredient.amount
             ? Color.white : Color.red;
+
+        icon.sprite = GroceryDataDB.GetData(ingredient.grocery).Icon;
     }
 }
