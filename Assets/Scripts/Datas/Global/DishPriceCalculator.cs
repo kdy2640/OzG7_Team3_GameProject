217249ecff98042df80ec9a10011c573c3b90c39
Doyeop Kim <rlaehduq2640@gmail.com>
2026-09-02 17:06:03 +0900

    Feat DishPrice

--- Assets/Scripts/Datas/Global/DishPriceCalculator.cs
+++ Assets/Scripts/Datas/Global/DishPriceCalculator.cs
@@ -3,11 +3,26 @@ public static class DishPriceCalculator
 {
     public static int BasicPriceCalculate(DishType dish)
     {
-        int cost;
         UpgradeDataDB.GetData(dish).TryGetRequiredCost(
-            GameManager.Instance.Upgrade.RuntimeLevel.Get(dish), 
-            out cost
-            );
+            GameManager.Instance.Upgrade.RuntimeLevel.Get(dish),
+            out int cost);
+
+        DishDataSO dishData = DishDataDB.GetData(dish);
+        MarketManager market = GameManager.Instance.Market;
+        int businessDay = market.MarketData.CurrentBusinessDay;
+        TasteType eventTaste =
+            market.FestivalCalendar.GetNowTaste(businessDay);
+        CategoryType eventCategory =
+            market.FestivalCalendar.GetNowCategory(businessDay);
+
+        bool matchesTaste = eventTaste != TasteType.Count
+            && dishData.Tastes == eventTaste;
+        bool matchesCategory = eventCategory != CategoryType.Count
+            && dishData.Category == eventCategory;
+
+        if (matchesTaste || matchesCategory)
+            cost += cost / 2;
+
         return cost;
     }
 
