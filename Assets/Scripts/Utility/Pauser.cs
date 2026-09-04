71931b7d213b413b3181b48b7461dee52f323c7b
Doyeop Kim <rlaehduq2640@gmail.com>
2026-09-04 17:48:46 +0900

    Feat SalesResultPanel

--- Assets/Scripts/Utility/Pauser.cs
+++ Assets/Scripts/Utility/Pauser.cs
@@ -8,6 +8,8 @@ public class Pauser : MonoBehaviour
     [SerializeField] UI_EventHandler openSettingButton;
     [SerializeField] UI_EventHandler closeSettingButton;
 
+    private Coroutine pauseCoroutine;
+
     private void OnEnable()
     {
         openSettingButton.AddUIEvent(OnOpenSetting);
@@ -22,19 +24,48 @@ public class Pauser : MonoBehaviour
 
     private void OnOpenSetting(PointerEventData data)
     {
-        StartCoroutine(PauseCo());
+        if (pauseCoroutine != null)
+            StopCoroutine(pauseCoroutine);
+
+        pauseCoroutine = StartCoroutine(PauseCo());
     }
 
     private void OnCloseSetting(PointerEventData data)
     {
+        Resume();
+    }
+
+    public void Resume()
+    {
+        if (pauseCoroutine != null)
+        {
+            StopCoroutine(pauseCoroutine);
+            pauseCoroutine = null;
+        }
+
         Time.timeScale = 1f;
     }
 
     private IEnumerator PauseCo()
     {
         yield return new WaitForSeconds(0.5f);
+
+        SceneType currentSceneType = GameManager.Instance.Scene.CurrentSceneType;
+        bool hasSessionEnded =
+            currentSceneType == SceneType.Service
+                && !GameManager.Instance.Service.IsRunning
+            || currentSceneType == SceneType.Harvest
+                && !GameManager.Instance.Harvest.IsRunning;
+
+        if (hasSessionEnded)
+        {
+            pauseCoroutine = null;
+            yield break;
+        }
+
         closeSettingButton = GameObject.Find("Close_Btn").GetComponent<UI_EventHandler>();
         closeSettingButton.AddUIEvent(OnCloseSetting);
         Time.timeScale = 0f;
+        pauseCoroutine = null;
     }
 }
