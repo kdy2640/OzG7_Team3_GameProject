da43f9b9c68420b0e24ead42999319913b47f066
JuTaeHyung <142490305+JuTaeHyung@users.noreply.github.com>
2026-09-04 18:06:12 +0900

    feat : VFX

--- Assets/Scripts/Service/Dirty/Dirty.cs
+++ Assets/Scripts/Service/Dirty/Dirty.cs
@@ -1,12 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Dirty : MonoBehaviour
 {
     private CustomerStateManager customer;
     public CustomerStateManager Customer => customer;
+
     
     public void SetCustomer(CustomerStateManager customer)
     {
         this.customer = customer;
     }
+
 }
